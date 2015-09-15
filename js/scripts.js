9d09ddaf39ef9837969a2b83075208ef157119b9
Kendra Ash & Carlo DeGregorio <student@epicodus.com>
2015-09-15 16:15:31 -0700

    allows the user to select movie, movie times, and and age. Prints a confirmation message to the screen

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,19 @@ Ticket.prototype.ticketPrice = function() {
   return (this.price = "14.00");
 }
 
-// $(document).ready(function(event) {
-//   event.preventDefault;
-//
-//   var inputNameOfMovie = $("input#new-movie-name");
-//   var inputAge = $("input#new-age");
-//   var inputShowtime = $("input#new-showtime");
-//   var newTicket = new Ticket(inputNameofMovie, inputAge, inputShowtime);
-// })
+$(document).ready(function(event) {
+
+  $("form#new-movie-ticket").submit(function(event) {
+    event.preventDefault();
+
+    var inputNameOfMovie = $("select#new-movie-name option:selected").text();
+    var inputAge = $("select#new-age").text();
+    var inputShowtime = $("select#new-showtime option:selected").text();
+    var newTicket = new Ticket(inputNameOfMovie, inputAge, inputShowtime);
+
+  $('#ticket-confirmation').text('You have successfuly purchased tickets for : ' + newTicket.nameOfMovie + " showing at " + newTicket.showtime);
+
+  });
+
+
+});
